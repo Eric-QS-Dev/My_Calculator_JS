e66885a6246f6b3cfe862c1bf7bdab1e23c4536c
Eric Queiroz <141651005+Eric-QS-Dev@users.noreply.github.com>
2026-07-02 15:16:16 -0300

    Adding JavaScript

---
+++ script.js
@@ -0,0 +1,22 @@
+function insertDisplay(data) {
+  document.querySelector('#display').value += data
+}
+
+function clean () {
+    document.querySelector('#display').value = ''
+}
+
+function del () {
+    const display = document.querySelector('#display')
+    display.value = display.value.slice(0, -1)
+ }
+
+function result () {
+        const display = document.querySelector('#display')
+        try {
+            display.value = eval(display.value).toFixed(2)
+        } catch { 
+            display.value = 'Error'
+     }
+
+}
